a38f0054b43876ff1f5172e7b33d91342c2af2b2
Alan <ldong@gatech.edu>
2017-08-05 00:28:34 -0700

    Done

--- README.md
+++ README.md
@@ -14,5 +14,15 @@ http://localhost:3000/users/sign_in
 rails c
 User.count
 
+
+
+gem 'devise'
+gem 'bootstrap-sass'
+gem 'acts_as_votable'
+gem 'simple_form'
+gem 'record_tag_helper'
+
+rails g migration add_name_to_users name:string
+rails db:migrate
 ```
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805065518) do
+ActiveRecord::Schema.define(version: 20170805072508) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170805065518) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
